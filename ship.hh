fc864edd55a15567b7f349f2be9a8a4e8016d4cb
themole <mole.coding@googlemail.com>
2011-05-28 09:00:34 +0200

    got rid of unneeded and old files

--- ship.hh
+++ ship.hh
@@ -1,22 +1,16 @@
 #ifndef SHIP_HH
 #define SHIP_HH
 
-#include "occupant.hh"
 #include "stock.hh"
 
-class Ship : public Occupant {
+class Ship {
 
 public:
 
     Stock & stock();
 
-    Stock cost() const;
-    void setCost( Stock const & cost ) const;
-
 protected:
     Stock _stock;
-    
-    static Stock _cost;
 };
 
 #endif // SHIP_HH
