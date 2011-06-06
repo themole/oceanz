9744f173d58bdc4a62a2742e574a49fbbdb08adc
themole <mole.coding@googlemail.com>
2011-06-06 22:36:49 +0200

    added simple Region class to hold more detailed region information than before possible

---
+++ region.cc
@@ -0,0 +1,54 @@
+#include "region.hh"
+
+unsigned Region::current_id = 0;
+
+Region::Region( region_type type )
+    : _id( Region::current_id++ ), 
+      _sz( 0 ),
+      _tp( type ) {
+}
+
+Region::~Region() {
+    // not responible for anything
+}
+
+unsigned
+Region::id() const {
+    return _id;
+}
+
+unsigned
+Region::size() const {
+    return _sz;
+}
+
+region_type
+Region::type() const {
+    return _tp;
+}
+
+std::set< Region* >
+Region::connected() const {
+    return _c;
+}
+
+void
+Region::setType( region_type type ) {
+    _tp = type;
+}
+
+void
+Region::setSize( unsigned size ) {
+    _sz = size;
+}
+
+void
+Region::setConnected( Region* r ) {
+    if( r->id() != _id )
+        _c.insert( r );
+}
+
+void
+Region::incrementSize() {
+    _sz++;
+}
---
+++ region.hh
@@ -0,0 +1,40 @@
+#ifndef REGION_HH
+#define REGION_HH
+
+#include <set>
+
+enum region_type { 
+    WATER,
+    LAND,
+    COAST
+};
+
+class Region {
+
+public:
+    Region( region_type type );
+    ~Region();
+
+    unsigned id() const;
+    unsigned size() const;
+    region_type type() const;
+
+    std::set< Region* > connected() const;
+
+    void setType( region_type type );
+    void setSize( unsigned size );
+    void setConnected( Region * r );
+
+    void incrementSize();
+
+private:
+    unsigned _id;
+    unsigned _sz;
+    region_type _tp;
+
+    std::set< Region* > _c; // connected Regions
+
+    static unsigned current_id;
+};
+
+#endif // REGION_HH
